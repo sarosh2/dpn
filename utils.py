a37e68457b903ed689ec1810b0990f94e949ac26
sarosh2 <saroshanish@gmail.com>
2025-06-08 14:07:59 +0300

    refactored MNIST test and gitignore

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ def evaluate(model, dataloader, criterion):
             loss = criterion(outputs, y_batch)
             loss_sum += loss.item() * y_batch.size(0)
             preds = outputs.argmax(dim=1)
-            y_batch_labels = y_batch
+            y_batch_labels = y_batch.argmax(dim=1) if y_batch.dim() > 1 else y_batch
             correct += (preds == y_batch_labels).sum().item()
             total += y_batch.size(0)
     return loss_sum / total, correct / total
@@ -44,12 +44,12 @@ def train(model, train_loader, val_loader, test_loader, epochs, optimizer, crite
             
             running_loss += loss.item() * y_batch.size(0)
             preds = outputs.argmax(dim=1)
-            y_batch_labels = y_batch
+            y_batch_labels = y_batch.argmax(dim=1) if y_batch.dim() > 1 else y_batch
             correct += (preds == y_batch_labels).sum().item()
             total += y_batch.size(0)
 
             batch_times.append(batch_time)
-            train_metrics.append((batch_time, loss.item(), (preds == y_batch).float().mean().item()))
+            train_metrics.append((batch_time, loss.item(), (preds == y_batch_labels).float().mean().item()))
 
         total_time = sum(batch_times)
         avg_time_per_batch = total_time / len(batch_times)
@@ -60,13 +60,18 @@ def train(model, train_loader, val_loader, test_loader, epochs, optimizer, crite
             val_loss, val_acc = evaluate(model, val_loader, criterion)
             val_metrics.append((val_loss, val_acc))
 
+        print("")
         print(f"Epoch: {epoch} Total_Time: {total_time:.4f} Average_Time_per_batch: {avg_time_per_batch:.4f} "
-            f"Train_Accuracy: {train_acc:.4f} Train_Loss: {train_loss:.4f} ")
+            f"Train_Accuracy: {train_acc:.4f} Train_Loss: {train_loss:.4f} ", end='')
+        
+        if val_loader:
+            print(f"Validation_Accuracy: {val_acc:.4f} Validation_Loss: {val_loss:.4f}", end='')
         
     # Finally test
     test_loss, test_acc = evaluate(model, test_loader, criterion)
     test_metrics.append((test_loss, test_acc))
 
+    print("")
     print(f"Test_Accuracy:  {test_acc:.4f} Test_Loss:  {test_loss}")
     print(f"Peak GPU memory: {torch.cuda.max_memory_allocated() / 1e6:.2f} MB")
 
